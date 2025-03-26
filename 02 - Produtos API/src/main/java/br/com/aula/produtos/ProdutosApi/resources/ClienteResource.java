fa27ad1f86d83a954599e0f440a42f9f853a7c30
Daves Martins Professor <daves@daves.com>
2025-03-25 22:29:40 -0300

    aula 25-03

--- 02 - Produtos API/src/main/java/br/com/aula/produtos/ProdutosApi/resources/ClienteResource.java
+++ 02 - Produtos API/src/main/java/br/com/aula/produtos/ProdutosApi/resources/ClienteResource.java
@@ -10,12 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -46,5 +44,49 @@ public class ClienteResource {
                     HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("")
+    public ResponseEntity<?> getAllClientes() {
+        ArrayList<Cliente> clientes = cs.getAllClientes();
+        if(clientes.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não há clientes cadastrados");
+        }
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/compras")
+    public ResponseEntity<?> getClienteComCompras() {
+        List<Cliente> clientes = cs.getClientesComCompras();
+        if(clientes.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não há clientes com compras");
+        }
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/compras/no")
+    public ResponseEntity<?> getClienteSemCompras() {
+        ArrayList<Cliente> clientes = cs.getClientesSemCompras();
+        if(clientes.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não há clientes sem compras");
+        }
+        return ResponseEntity.ok(clientes);
+    }
+
+    @PostMapping("/{idCliente}/compras/{idProduto}/{qtde}")
+    public ResponseEntity<?> comprarProduto(@PathVariable Integer idCliente,
+                                            @PathVariable Integer idProduto,
+                                            @PathVariable Integer qtde ) {
+        try {
+            cs.realizaCompra(idCliente, idProduto, qtde);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(e.getMessage());
+        }
+        return ResponseEntity.ok("Compra realizada com sucesso");
+    }
+
 
 }
--- 02 - Produtos API/src/main/java/br/com/aula/produtos/ProdutosApi/service/ClienteService.java
+++ 02 - Produtos API/src/main/java/br/com/aula/produtos/ProdutosApi/service/ClienteService.java
@@ -2,11 +2,13 @@ package br.com.aula.produtos.ProdutosApi.service;
 
 import br.com.aula.produtos.ProdutosApi.ProdutosApiApplication;
 import br.com.aula.produtos.ProdutosApi.model.Cliente;
+import br.com.aula.produtos.ProdutosApi.model.Produto;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ClienteService {
@@ -24,5 +26,55 @@ public class ClienteService {
         for (Cliente cliente : clientes) {
             saveCliente(cliente);
         }
+    }
+
+    public ArrayList<Cliente> getAllClientes() {
+        return ProdutosApiApplication.clientes;
+    }
+
+    public ArrayList<Cliente> getClientesSemCompras() {
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        for (Cliente cliente : ProdutosApiApplication.clientes) {
+            if (cliente.getProdutos() == null || cliente.getProdutos().isEmpty()) {
+                clientes.add(cliente);
+            }
+        }
+        return clientes;
+    }
+
+
+    public List<Cliente> getClientesComCompras() {
+        return ProdutosApiApplication.clientes.stream()
+                .filter( c -> c.getProdutos()!= null && c.getProdutos().size()>0)
+                .toList();
+    }
+
+    public boolean realizaCompra(Integer idCliente, Integer idProduto, Integer qtde) throws Exception {
+        if (!searchCliente(idCliente)){
+            throw new Exception("Cliente "+idCliente+" não existe");
+        }
+        if (!searchProduto(idProduto)){
+            throw new Exception("Produto "+idProduto+" não existe");
+        }
+        Produto p = ProdutosApiApplication.produtos.get(idProduto);
+        if (p.getQuantidadeEstoque() < qtde){
+            throw new Exception("Produto "+idProduto+" não tem estoque suficiente");
+        }
+        Cliente c = ProdutosApiApplication.clientes.get(idCliente);
+        if (c.getProdutos() == null){
+            c.setProdutos(new ArrayList<>());
+        }
+        c.getProdutos().add(p);
+        p.setQuantidadeEstoque( p.getQuantidadeEstoque() - qtde);
+
+        return true;
+    }
+
+    private boolean searchProduto(Integer idProduto) {
+        return ProdutosApiApplication.produtos.size() > idProduto;
+    }
+
+    private boolean searchCliente(Integer idCliente) {
+        return ProdutosApiApplication.clientes.size() > idCliente;
     }
 }
