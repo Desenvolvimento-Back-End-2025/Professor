a79b1cc2c13e74e9f4a951146dad25deebab6bfe
Daves Martins Professor <daves@daves.com>
2025-03-27 21:24:23 -0300

    aula 27-03

---
+++ 03 - UniAcademy/src/main/java/br/edu/academy/UniAcademy/UniAcademyApplication.java
@@ -0,0 +1,13 @@
+package br.edu.academy.UniAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniAcademyApplication.class, args);
+	}
+
+}
---
+++ 03 - UniAcademy/src/test/java/br/edu/academy/UniAcademy/UniAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.academy.UniAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
